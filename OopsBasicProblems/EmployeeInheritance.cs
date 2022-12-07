a4dd76b9f3f264f5353a6df81a21d1f987120659
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-07 19:25:57 +0530

    [Mahesh]Add: Single Inheritance

---
+++ OopsBasicProblems/EmployeeInheritance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OopsBasicProblems
+{
+    public class EmployeeInheritance
+    {
+        public string companyName;
+    }
+    public class EmployeeInheritance2 : EmployeeInheritance
+    {
+        public string name;
+        public int empID;
+        public EmployeeInheritance2(string name, int empID, string companyName)
+        {
+            this.name = name;
+            this.empID = empID;
+            this.companyName = companyName;
+        }
+        public void Check()
+        {
+            Console.WriteLine("Employee name " + name);
+            Console.WriteLine("Employee id " + empID);
+            Console.WriteLine("Employee companyName  " + companyName);
+
+
+        }
+
+    }
+}
--- OopsBasicProblems/Program.cs
+++ OopsBasicProblems/Program.cs
@@ -1,12 +1,25 @@
-﻿/*//In C#, inheritance is a process 
- * 
- * C# Inheritance
- * in which one object acquires all the properties and behaviors of its parent object automatically. In such way, you can reuse, extend or modify the attributes and behaviors which is defined in other class.
-
-In C#, the class which inherits t//he members of another class is called derived class and the class whose members are inherited is called base class. The derived class is the specialized class for the base class
-
-C# Single Level Inheritance Example: Inheriting Fields
-When one class inherits another class, it is known as single level inheritance. Let's see the example of single level inheritance which inherits the fields only.
-
-C# Multi Level Inheritance Example
-When one class inherits another class which is further inherited by another class, it is known as multi level inheritance in C#. Inheritance is transitive so the last derived class acquires all the members of all its base cla.*/
+﻿
+using System;
+namespace OopsBasicProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("For Understanding Basic Inheritance");
+            Console.WriteLine("Click Below options :" + "\n" + "1.Singleinheritance");
+            while (true) 
+            {
+                int option =Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        EmployeeInheritance2 e1 = new EmployeeInheritance2("Mahesh", 5009, "Wistron Private Limited");
+                        e1.Check();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
