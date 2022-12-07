8c9e6dd796d74a1e1c704bda3384642a88ff6d47
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-07 21:46:28 +0530

    [Mahesh]Add: Multiple Inheritance

---
+++ OopsBasicProblems/Animals.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsBasicProblems
+{
+    public class Animals
+    {
+        public void standing()
+        {
+            Console.WriteLine("All Animals Are standing for Looking to each them faces");
+        }
+    }
+    public class MaleDog : Animals
+    {
+        string color = "White";
+        string dogNames = "RRRR";
+        int noOfDogs = 6;
+        public void Dogstanding()
+        {
+            Console.WriteLine("Rajamouli is  DOGS manager . \n He simply watcing dogs details for marriage  :- ");
+            Console.WriteLine("MaleDog name " + dogNames + " them Color " + color + " and there are " + noOfDogs + " waiting for marriage.");
+        }
+    }
+    public class FemaleDog : Animals
+    {
+        string color1;
+        string dogName1;
+        int noOfDogs1;
+        public FemaleDog(string color1, string dogName1, int noOfDogs1)
+        {
+            this.color1 = color1;
+            this.dogName1 = dogName1;
+            this.noOfDogs1 = noOfDogs1;
+        }
+        public void Waiting()
+        {
+            Console.WriteLine("Also Rajmouli looking Femaledogs Detalis :-");
+            Console.WriteLine("Female dogs names " + dogName1 + " them Color " + color1 + " and there are " + noOfDogs1 + " simply sitting in the Hall.\n Remaing one Maledog is SINGLE ");
+        }
+    }
+
+
+
+}
--- OopsBasicProblems/Program.cs
+++ OopsBasicProblems/Program.cs
@@ -7,7 +7,7 @@ namespace OopsBasicProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("For Understanding Basic Inheritance");
-            Console.WriteLine("Click Below options :" + "\n" + "1.Singleinheritance");
+            Console.WriteLine("Click Below options :" + "\n" + "1.Singleinheritance"+"\n"+"2.MultipleInheritance");
             while (true) 
             {
                 int option =Convert.ToInt32(Console.ReadLine());
@@ -16,6 +16,14 @@ namespace OopsBasicProblems
                     case 1:
                         EmployeeInheritance2 e1 = new EmployeeInheritance2("Mahesh", 5009, "Wistron Private Limited");
                         e1.Check();
+                        break;
+                    case 2:
+                        Animals a1 = new Animals();
+                        a1.standing();
+                        MaleDog m1 = new MaleDog();
+                        m1.Dogstanding();
+                        FemaleDog e2 = new FemaleDog("Red","SSSS",5);
+                        e2.Waiting();
                         break;
                 }
             }
