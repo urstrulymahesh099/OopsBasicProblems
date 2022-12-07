f9b4b7dae89ffc1009a5b5061f2691b6a1b55ede
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-07 19:16:05 +0530

    [Mahesh]Add: Inheritance folder structure is created

---
+++ OopsBasicProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿/*//In C#, inheritance is a process 
+ * 
+ * C# Inheritance
+ * in which one object acquires all the properties and behaviors of its parent object automatically. In such way, you can reuse, extend or modify the attributes and behaviors which is defined in other class.
+
+In C#, the class which inherits t//he members of another class is called derived class and the class whose members are inherited is called base class. The derived class is the specialized class for the base class
+
+C# Single Level Inheritance Example: Inheriting Fields
+When one class inherits another class, it is known as single level inheritance. Let's see the example of single level inheritance which inherits the fields only.
+
+C# Multi Level Inheritance Example
+When one class inherits another class which is further inherited by another class, it is known as multi level inheritance in C#. Inheritance is transitive so the last derived class acquires all the members of all its base cla.*/
